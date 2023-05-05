da429b79e891394d58de088ab9ad3ffc39ef7f63
juh3 <juha.tapio20@gmail.com>
2023-05-05 15:52:58 +0300

    feat: modal opening animation

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -2,6 +2,8 @@ import React, { useCallback, useEffect, useState } from 'react'
 import styles from './Modal.module.scss'
 import {AiOutlineClose} from 'react-icons/ai'
 import Image from 'next/image'
+import { motion } from 'framer-motion'
+
 interface ModalProps{
   isOpen: Boolean
   onClose: () => void
@@ -23,7 +25,6 @@ const Modal: React.FC<ModalProps> = ({
     setShowModal(isOpen)
   }, [isOpen])
 
-  console.log(showModal, 'showmodal')
   const handleClose = useCallback(() => {
     setShowModal(false)
     setTimeout(() => {
@@ -31,15 +32,21 @@ const Modal: React.FC<ModalProps> = ({
     }, 300)
   }, [onClose])
   return (
+    
     <div className={styles.modalContainer}>
+      
       <div className={styles.modalWrapper}>
         {showModal && 
-          <div className={styles.modalContent}>
-            <div className={styles.modalTopBar}>
-              <button onClick={handleClose} className={styles.modalClose}><AiOutlineClose size={28} color='black'/>
-              </button>
-              
-            </div>
+          <motion.div 
+            initial = {{opacity:0, translateY: '100%'}}
+            animate = {{ opacity: 1, translateY: 0}}
+            transition={{ duration: 0.3}}>
+            <div className={styles.modalContent}>
+              <div className={styles.modalTopBar}>
+                <button onClick={handleClose} className={styles.modalClose}><AiOutlineClose size={28} color='black'/>
+                </button>
+              </div>
+
             <div className={styles.modalHeader}>
               <h3 style={{color: 'black'}}>Lorem ipsum dolor</h3>
             </div>
@@ -52,15 +59,16 @@ const Modal: React.FC<ModalProps> = ({
               <p className={styles.bodyText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
 
+            <div className = {styles.modalButtonContainer}>
+              <button className={styles.modalButton}>To the site</button>
+            </div>
             <div>
               <hr style={{marginTop: '8px', marginBottom: '24px'}}/>
               </div>
-          </div>}
-          {!showModal&& 
-            <div style={{opacity:'0'}}>
               </div>
-          }
-      </div>
+          </motion.div>}
+     
+          </div>
     </div>
   )
 }
