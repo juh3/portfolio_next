449dd21b3754238533e68af666804914bc6860d6
Juha <juha.tapio20@gmail.com>
2023-09-21 00:22:46 +0300

    image animation finally works

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,91 +6,126 @@ import juhaplaceholder from '../public/juhaplaceholder.jpg'
 import Image from 'next/image'
 
 const Header = () => {
-  const textRef = useRef<HTMLSpanElement>(null)
+    const textRef = useRef<HTMLSpanElement>(null)
 
-  const welcometext = {
-    whileInView: {
-      x: '200px',
-      transition: { duration: 2, ease: 'easeOut' },
-    },
-  }
-
-  const welcomename = {
-    whileInView: {
-      x: '200px',
-      transition: { duration: 3, ease: 'easeOut' },
-    },
-  }
-
-
-  useEffect(() => {
-    if (!textRef.current) {
-      return;
+    const welcometext = {
+        whileInView: {
+            x: '200px',
+            transition: { duration: 2, ease: 'easeOut' },
+        },
     }
-    init(textRef.current, {
-      showCursor: false,
-      strings: [
-        'UX/UI Design',
-        'Frontend development',
-        'User Research',
-        'Design',
-        'Coffee',
-        'Houseplants!',
-      ],
-    } )
-  }, [])
 
-  const card = {
-    initial: { backgroundColor: "#000000"},
-    hover: { backgroundColor: "#1C1C1C", transition: {ease: "easeIn"}}
-  }
-  const cover = {
-    initial: { left: 0, bottom: 0},
-    hover: { left: 32, bottom: 32}
+    const welcomename = {
+        whileInView: {
+            x: '200px',
+            transition: { duration: 3, ease: 'easeOut' },
+        },
+    }
 
-  }
-  const cover2 = {
-    initial: { left: 0, bottom: 0},
-    hover: { left: 16, bottom: 16}
+    useEffect(() => {
+        if (!textRef.current) {
+            return
+        }
+        init(textRef.current, {
+            showCursor: false,
+            strings: [
+                'UX/UI Design',
+                'Frontend development',
+                'User Research',
+                'Design',
+                'Coffee',
+                'Houseplants!',
+            ],
+        })
+    }, [])
 
-  }
-  return (
-    <div className={styles.app__header_style} id="Home">
-      <div className={styles.left}>
-        <div className={styles.wrapper}>
-          <motion.div
-            variants={welcometext}
-            whileInView={welcometext.whileInView}
-          >
-            <h1 className={styles.greeting}> Hey!</h1>
-          </motion.div>
+    const cover = {
+        whileInView: {
+            left: [0, 32, 0],
+            bottom: [0, 32, 0],
+            transition: {
+                ease: 'easeInOut',
+                duration: 5,
+                repeatDelay: 2,
+                repeat: Infinity,
+            },
+        },
+    }
+    const cover2 = {
+        whileInView: {
+            left: [0, 16, 0],
+            bottom: [0, 16, 0],
+            transition: {
+                ease: 'easeInOut',
+                duration: 5,
+                repeatDelay: 2,
+                repeat: Infinity,
+            },
+        },
+    }
+    const card = {
+        initial: { opacity: 1 },
+        whileInView: {
+            transition: {
+                ease: 'easeInOut',
+                duration: 5,
+                repeatDelay: 2,
+                repeat: Infinity,
+            },
+        },
+    }
 
-          <motion.div
-            variants={welcomename}
-            whileInView={welcomename.whileInView}
-            style={{ padding: '0.1rem' }}
-          >
-            <h1 className={styles.name}> I am Juha</h1>
-            <h3 className={styles.interests}>
-              {' '}
-              Interested in: <br></br>
-              <span id = {styles.span} ref={textRef}></span>{' '}
-            </h3>
-          </motion.div>
+    return (
+        <div className={styles.app__header_style} id="Home">
+            <div className={styles.left}>
+                <div className={styles.wrapper}>
+                    <motion.div
+                        variants={welcometext}
+                        whileInView={welcometext.whileInView}
+                    >
+                        <h1 className={styles.greeting}> Hey!</h1>
+                    </motion.div>
+
+                    <motion.div
+                        variants={welcomename}
+                        whileInView={welcomename.whileInView}
+                        style={{ padding: '0.1rem' }}
+                    >
+                        <h1 className={styles.name}> I am Juha</h1>
+                        <h3 className={styles.interests}>
+                            {' '}
+                            Interested in: <br></br>
+                            <span id={styles.span} ref={textRef}></span>{' '}
+                        </h3>
+                    </motion.div>
+                </div>
+            </div>
+
+            <div className={styles.right}>
+                <motion.div
+                    className={styles.card}
+                    variants={card}
+                    initial="initial"
+                    whileInView={card.whileInView}
+                >
+                    <motion.div className={styles.squarecards}>
+                        <motion.img
+                            src="juhaplaceholder.jpg"
+                            className={styles.card_cover}
+                            whileInView={cover.whileInView}
+                        ></motion.img>
+                        <motion.div
+                            className={styles.card_back_one}
+                            whileInView={cover2.whileInView}
+                        ></motion.div>
+                        <motion.div
+                            className={styles.card_back_two}
+                        ></motion.div>
+                    </motion.div>
+                </motion.div>
+            </div>
         </div>
-      </div>
-
-      <div className={styles.right}>
-        <div className={styles.card}>
-          <div className={styles.squarecards}>
-          <motion.img src = '/public/juhaplaceholder.jpg' className={styles.card_cover} variants={card} initial = "initial" whileHover = "hover"></motion.img>
-          <motion.div className={styles.card_back_one}></motion.div>
-          <motion.div className={styles.card_back_two}></motion.div>
-          </div>
-        </div>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Header
