ff19beef7680e20fa312216c3ba45d64a5da0651
juh3 <juha.tapio20@gmail.com>
2023-09-21 15:08:08 +0300

    some cards

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,12 +2,28 @@ import React from 'react'
 import styles from './Card.module.scss'
 import useProjectModal from '../hooks/useProjectModal'
 import Image from 'next/image'
-const Card = () => {
-  const rootModal = useProjectModal()
+
+interface CardInterface {
+  title: string,
+  description: string,
+  link: string,
+  image: string
+}
+
+const Card:React.FC<CardInterface> = ({title,description, link, image}) => {
+  const handleCardClick = () => {
+    window.open(link, '_blank')
+  }
   return (
-    <div className={styles.card}>
-      <div className={styles.card_image}>
-        <Image src="/ballofroots_frontpage.png" alt="ball of roots logo" width={600} height={600}/>
+    <div className={styles.card} onClick={handleCardClick}>
+      <div className={styles.card_overlay}>
+        <div className={styles.card_image}>
+          <Image src={image} alt="ball of roots logo" width={600} height={600}/>
+        </div>
+        <div className={styles.card_description}>
+          <h2>{title}</h2>
+          <p>{description}</p>
+        </div>
       </div>
     </div>
   )
--- src/components/Work/Work.tsx
+++ src/components/Work/Work.tsx
@@ -1,9 +1,73 @@
 import React from 'react'
 import styles from './Work.module.scss'
 import Card from '../Card/Card'
-import { motion} from 'framer-motion'
+import { useRef } from "react";
+import {
+  motion,
+  useSpring,
+  useTransform,
+  useScroll,
+  useMotionValue,
+  useVelocity,
+  useAnimationFrame
+} from "framer-motion";
+import { wrap } from "@motionone/utils";
 import Image from 'next/image'
 
+interface ParallaxProps {
+  children: string;
+  baseVelocity: number;
+}
+
+function ParallaxText({ children, baseVelocity = 100 }: ParallaxProps) {
+  const baseX = useMotionValue(0);
+  const { scrollY } = useScroll();
+  const scrollVelocity = useVelocity(scrollY);
+  const smoothVelocity = useSpring(scrollVelocity, {
+    damping: 50,
+    stiffness: 300
+  });
+  const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 20], {
+    clamp: false
+  });
+
+  /**
+   * This is a magic wrapping for the length of the text - you
+   * have to replace for wrapping that works for you or dynamically
+   * calculate
+   */
+  const x = useTransform(baseX, (v) => `${wrap(20, -20, v)}%`);
+
+  const directionFactor = useRef<number>(1);
+  useAnimationFrame((t, delta) => {
+    let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
+      directionFactor.current = -1;
+    
+
+    moveBy += directionFactor.current * moveBy * velocityFactor.get();
+
+    baseX.set(baseX.get() + moveBy);
+  });
+
+  /**
+   * The number of times to repeat the child text should be dynamically calculated
+   * based on the size of the text and viewport. Likewise, the x motion value is
+   * currently wrapped between -20 and -45% - this 25% is derived from the fact
+   * we have four children (100% / 4). This would also want deriving from the
+   * dynamically generated number of children.
+   */
+  return (
+    <div className="parallax">
+      <motion.div className="scroller" style={{ x }}>
+        <span>{children} </span>
+        <span>{children} </span>
+        <span>{children} </span>
+        <span>{children} </span>
+        <span>{children} </span>
+      </motion.div>
+    </div>
+  );
+}
 const Work = () => {
 
   const projecttext = {
@@ -16,13 +80,15 @@ const Work = () => {
   
   return (
     <div className={styles.work_container} id="Work">
-      <motion.div whileInView={projecttext.whileInView} className={styles.work_wrapper}>
-        <h1 className={styles.work_h1}> Projects</h1>
-      </motion.div>
+      <section>
+      <ParallaxText baseVelocity={-5}>Projects</ParallaxText>
+      <ParallaxText baseVelocity={5}>Projects</ParallaxText>
+
+    </section>
       <motion.div className={styles.projects}>
-        <Card/>
-        <Card/>
-        <Card/>
+        <Card title="Ball of Roots" image ="/ballofroots_frontpage.png" description='My first project after completing the FullStack Open course. Stack included React, Node, and MongoDB' link="https://ballofroots.com/"/>
+        <Card title="Kinopoli" image = "/kinopoli_frontpage.png" description='I was part of the dev team focusing on the frontend. Stack included React, Next, Vite, SCSS' link="https://kinopoli.fi/"/>
+        <Card title = "ApproKartat" image = "/ballofroots_frontpage.png" description='My first project' link="https://ballofroots.com/"/>
       </motion.div>
     </div>
   )
