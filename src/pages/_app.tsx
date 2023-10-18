aa9506f1d4a931c9b98e54c8689960aad0d38129
juh3 <juha.tapio20@gmail.com>
2023-10-18 17:57:51 +0300

    something

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,15 @@ import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
+    <div
+      style={{
+        padding: 0,
+        margin: 0,
+        width: '100vw',
+        minHeight: '100vh',
+        position: 'relative'
+      }}
+    >
       <Navbar />
       <Component {...pageProps} />
     </div>
