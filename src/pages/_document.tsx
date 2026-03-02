d7aecaf805bb5d0b1ad96694c04c9bcdf11d6d04
Juh3 <juha.tapio20@gmail.com>
2026-03-02 12:58:53 +0200

    add vercel insights

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,21 +1,26 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export default function Document() {
   return (
-    <Html style={{scrollBehavior:'smooth'}} lang="en">
-       <link
-      href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap"
-      rel="stylesheet"
-    />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Raleway:wght@200&display=swap"
-      rel="stylesheet"
-    />
-    <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@500&display=swap" rel="stylesheet"/>
+    <Html style={{ scrollBehavior: 'smooth' }} lang="en">
+      <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Raleway:wght@200&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@500&display=swap"
+        rel="stylesheet"
+      />
       <Head />
       <body>
         <Main />
         <NextScript />
+        <SpeedInsights />
       </body>
     </Html>
   )
